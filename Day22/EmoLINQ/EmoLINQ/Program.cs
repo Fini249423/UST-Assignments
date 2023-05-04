5f0570ea1051d73b9da1b74c878c0a2062b5b5f5
Fini249423 <129931696+Fini249423@users.noreply.github.com>
2023-05-04 18:07:46 +0530

    Add files via upload

---
+++ Day22/EmoLINQ/EmoLINQ/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmoLINQ
+{
+    public class Employee
+    {
+        private string _name;
+        public string Name { get { return _name; } set { _name = value;} }
+
+        private string _city;
+        public string City { get { return _city; } set { _city = value; } }
+
+        public Employee(string name,string city)
+        {
+            Name = name;
+            City = city;
+        }
+        public Employee()
+        {
+
+        }
+
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+                List<Employee> EmpList = new List<Employee>();
+
+
+            EmpList.Add(new Employee("Arun","Hyd"));
+            EmpList.Add(new Employee("Rahul","Trv"));
+            EmpList.Add(new Employee("Anu","Delhi"));
+            EmpList.Add(new Employee("Sree","Karnataka"));
+            EmpList.Add(new Employee("Ben", "Ernklm"));
+            EmpList.Add(new Employee("Amritha","Delhi"));
+            EmpList.Add(new Employee("Manu","Trv"));
+
+            var Query = from Employee emp in EmpList
+                        orderby emp.City
+                        group emp by emp.City;
+            foreach(var e in Query)
+            {
+                Console.WriteLine("Employee with " + e.Key + " City:");
+                foreach(var em in e)
+                {
+                    Console.WriteLine("" + em.Name);
+                }
+            }
+
+                
+        }
+
+    }
+}
---
+++ Day22/StringLINQ/StringLINQ/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLINQ
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] flowers = { "dahlia", "rose", "lotus", "lily", "hibiscus", "daffodil" };
+            var fQuery=
+                from flower in flowers
+                where (flower.StartsWith("d"))
+                select flower;           
+            foreach(string f in fQuery)
+            {
+                Console.WriteLine(f);
+            }
+            Console.WriteLine();
+            var lquery = 
+                from flower in flowers
+                let len=flower.Length
+                where (flower.StartsWith("d")&& len >6)
+                select flower;
+            foreach (string l in fQuery)
+            {
+                Console.WriteLine(l);
+            }
+        }
+    }
+}
