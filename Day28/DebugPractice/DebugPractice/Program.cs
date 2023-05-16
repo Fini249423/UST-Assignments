fe9a3c55ce71fb16b7258b214442ce793855c427
Fini249423 <129931696+Fini249423@users.noreply.github.com>
2023-05-16 10:21:48 +0530

    Add files via upload

---
+++ Day28/DebugPractice/DebugPractice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+//Arrange following lines in proper order to make program run:
+public class Product
+{
+    public string _supplierName;
+    public string _productName;
+
+    public long _id;
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Product p = new Product();
+        Console.WriteLine("Enter the product name");
+        p._productName = Console.ReadLine();
+
+        Console.WriteLine("Enter the product id");
+        p._id = Convert.ToInt64(Console.ReadLine());
+
+        Console.WriteLine("Enter the supplier name");
+        p._supplierName = Console.ReadLine();
+
+
+        Console.WriteLine("Product Id is " + p._id);
+        Console.WriteLine("Product Name is " + p._productName);
+        Console.WriteLine("Supplier Name is "+p._supplierName);
+
+    }
+
+}
+
+ 
---
+++ Day28/RazorSyntaxDemo/RazorSyntaxDemo/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace RazorSyntaxDemo.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name,int age)
+        {
+            Name = name;    
+            Age = age;  
+        }
+    }
+    
+        
+
+}
