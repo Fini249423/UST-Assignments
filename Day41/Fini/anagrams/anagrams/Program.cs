379af0d9b04bcb1a2b4aea148bbcd5ebb0cb01a0
Fini249423 <129931696+Fini249423@users.noreply.github.com>
2023-05-29 09:32:10 +0530

    Add files via upload

---
+++ Day41/Fini/anagrams/anagrams/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace Anagram
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Enter the first string ");
+
+            string a = Console.ReadLine();
+
+            Console.WriteLine("Enter the second string");
+
+            string b = Console.ReadLine();
+
+            if (a.Length == b.Length)
+
+            {
+
+                char[] arr1 = a.ToLower().ToCharArray();
+
+                char[] arr2 = b.ToLower().ToCharArray();
+
+                Array.Sort(arr1);
+
+                Array.Sort(arr2);
+
+                string res1 = new string(arr1);
+
+                string res2 = new string(arr2);
+
+                if (res1 == res2)
+
+                {
+
+                    Console.WriteLine("They are anagrams");
+
+                }
+
+                else
+
+                {
+
+                    Console.WriteLine("They are not anagrams");
+
+                }
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("They are not anagrams");
+
+            }
+
+        }
+
+    }
+
+}
